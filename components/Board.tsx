196e328e22ccf6205a35d1cc05c9de6bd63fee30
David Lazaro <dlazaro7@gmail.com>
2023-10-17 14:44:13 +0200

    ranks from ascii method implemted

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react";
 import styles from "./board.module.scss";
+import { Chess } from "chess.js";
 
 const Board = () => {
+  // represent the board
+  const [pieces, setPieces] = useState(
+    new Array(8).fill(0).map(() => new Array(8).fill(""))
+  );
+
+  useEffect(() => {
+    // new Game
+    const chess = new Chess();
+    // ascii square with lanes removed
+    const ranks = chess.ascii().split('\n').slice(1, 8)
+    console.log(ranks)
+  }, []);
+
   return (
     <div className={styles.board}>
       {new Array(8).fill(0).map((_, i) => {
