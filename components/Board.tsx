d19ece35c29bdb709a331f059fd210d2ee48f8cc
David Lazaro <dlazaro7@gmail.com>
2023-11-07 12:33:37 +0100

    chess renewed from my-portfolio

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,32 +1,31 @@
-import { useEffect, useRef, useState } from "react";
 import styles from "./board.module.scss";
-import { chess, getBoard, ranks, files } from "../utils/chess-utils";
-import { Move } from "chess.js";
+import { useEffect, useState, useRef } from "react";
+import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
 import { calculateBestMove, initGame } from "chess-ai";
-import Loader from "./Loader";
-import Cell from "./Cell";
+import { useMediaQuery } from "react-responsive";
 
 const Board = () => {
-
-  // pieces of the game
-  const [pieces, setPieces] = useState<string[][]>(
-    ranks.map(() => new Array(8).fill(" "))
-  );
   // web worker
   const workerRef = useRef<Worker>();
-  // posible next movements
-  const [highlighted, setHighlighted] = useState<(string | undefined)[]>([]);
-  // AI delay
-  const [isLoading, setIsLoading] = useState(false);
-  // player turn
-  const [turn, setTurn] = useState("w");
-  // king in check
-  const [inCheck, setInCheck] = useState(false);
-  // pawn promoted to queen
-  const [movesWithPromotion, setMovesWithPromotion] = useState<string[]>([]);
-  // two players game or AI
-  const isTwoPlayer = true;
-  
+  // mQuery for chess
+  const isPhone = useMediaQuery({
+    query: "(max-width: 1200px)",
+  });
+  const classBtn: string = isPhone ? styles.chessBtnPhone : styles.chessBtn;
+  // ai-skill (0-2)
+  const [aiSkill, setAiSkill] = useState<number>(0);
+
+  const [easyBtn, setEasyBtn] = useState<string>(classBtn);
+  const [mediumBtn, setMediumBtn] = useState<string>(classBtn);
+  const [hardBtn, setHardBtn] = useState<string>(classBtn);
+
+  // inCheck Alert
+  const [inCheckAlert, setInCheckAlert] = useState<string>(styles.bordered);
+  // new Game
+
+  const [game, setGame] = useState<any>(new Chess());
+
   useEffect(() => {
     workerRef.current = new Worker(
       new URL("../utils/worker.ts", import.meta.url)
@@ -35,94 +34,187 @@ const Board = () => {
       console.log("Hi from UI", e);
     };
     workerRef.current.postMessage("Hi -- from UI");
-    initGame(chess, 1);
-    setPieces(getBoard());
+    initGame(game, aiSkill); // From 0 - 2 ai-level
+
     return () => {
       workerRef.current?.terminate();
     };
-  }, []);
+  }, [game, aiSkill]);
 
-  function makeMove(mv: any, isAI: boolean) {
-    let move;
-    if (!isAI && movesWithPromotion.includes(mv.to)) {
-      chess.move({ ...mv, promotion: "q" });
-    } else move = chess.move(mv);
-    setPieces(getBoard());
-    setHighlighted([move?.to, move?.from]);
-    setIsLoading(!isAI);
-    setTurn(chess.turn());
-    setInCheck(chess.inCheck());
-    if (chess.isGameOver()) {
-      setTimeout(() => {
-        alert("Game Over");
-        chess.reset();
-        setTurn("b");
-        setInCheck(false);
-        setPieces(getBoard());
-        setHighlighted([]);
-        setIsLoading(false);
-      }, 100);
-    }
-  }
-
-  function handleCellClick(square: string, shouldGetMoves: boolean) {
-    if (highlighted.slice(1).includes(square)) {
-      makeMove(
-        {
-          to: square,
-          // @ts-ignore
-          from: highlighted[0],
-        },
-        false
-      );
-      if (isTwoPlayer) {
-        setIsLoading(false);
-      } else
-        setTimeout(() => {
-          const aiMove = calculateBestMove();
-          if (aiMove) makeMove(aiMove, true);
-        }, 200);
-    } else if (shouldGetMoves) {
-      const mvs = chess.moves({
-        // @ts-ignore
-        square,
-        verbose: true,
-      }) as Move[];
-      setHighlighted([square, ...mvs.map(({ to }) => to)]);
-      setMovesWithPromotion(
-        mvs.filter(({ flags }) => flags.includes("p")).map(({ to }) => to)
-      );
+  // Dificulty
+  const easy = () => {
+    if (isPhone) {
+      setEasyBtn(styles.activeBtnPhone);
+      setMediumBtn(styles.chessBtnPhone);
+      setHardBtn(styles.chessBtnPhone);
     } else {
-      setHighlighted([]);
+      setEasyBtn(styles.activeBtn);
+      setMediumBtn(styles.chessBtn);
+      setHardBtn(styles.chessBtn);
     }
+
+    setAiSkill(0);
+    game.reset();
+  };
+
+  const medium = () => {
+    if (isPhone) {
+      setEasyBtn(styles.chessBtnPhone);
+      setMediumBtn(styles.activeBtnPhone);
+      setHardBtn(styles.chessBtnPhone);
+    } else {
+      setEasyBtn(styles.chessBtn);
+      setMediumBtn(styles.activeBtn);
+      setHardBtn(styles.chessBtn);
+    }
+
+    setAiSkill(1);
+    game.reset();
+  };
+
+  const hard = () => {
+    if (isPhone) {
+      setEasyBtn(styles.chessBtnPhone);
+      setMediumBtn(styles.chessBtnPhone);
+      setHardBtn(styles.activeBtnPhone);
+    } else {
+      setEasyBtn(styles.chessBtn);
+      setMediumBtn(styles.chessBtn);
+      setHardBtn(styles.activeBtn);
+    }
+
+    setAiSkill(2);
+
+    game.reset();
+  };
+  const reset = () => {
+    setAiSkill(aiSkill);
+    game.reset();
+    //@ts-ignore
+    onDrop();
+  };
+  const undo = () => {
+    setAiSkill(aiSkill);
+    game.undo();
+    game.undo();
+    // @ts-ignore
+    onDrop();
+  };
+
+  //Let's perform a function on the game state
+
+  function safeGameMutate(modify: any) {
+    setGame((g: any) => {
+      const update = { ...g };
+      modify(update);
+      return update;
+    });
   }
+  //Movement of computer
+  function makeRandomMove() {
+    const possibleMove = game.moves();
+    // 1. Draw
+    if (game.in_draw() || game.in_stalemate()) {
+      alert("Draw");
+    }
+    // 2. Check Mate
+    if (game.in_checkmate() || possibleMove.length === 0) {
+      alert("Check Mate");
+    }
+
+    // 2.1
+    if (!game.in_check() && possibleMove.length === 0) {
+      alert("Draw");
+      game.game_over();
+    }
+    // 3. Game Over
+    if (game.game_over()) {
+      alert("Game Over");
+    }
+    //select ai move
+    const aiMove = calculateBestMove();
+
+    //play random move
+    safeGameMutate((game: any) => {
+      game.move(aiMove);
+    });
+
+    // onCheck?
+    game.in_check()
+      ? setInCheckAlert(styles.onCheck)
+      : setInCheckAlert(styles.bordered);
+  }
+
+  //Perform an action when a piece is droped by a user
+
+  function onDrop(source: string, target: string) {
+    let move = null;
+
+    safeGameMutate((game: any) => {
+      move = game.move({
+        from: source,
+        to: target,
+        promotion: "q",
+      });
+    });
+
+    // 1. illegal move
+    if (move == null) return false;
+    // 2. move for ai
+    setTimeout(makeRandomMove, 200);
+
+    return true;
+  }
+
   return (
-    <div
-      className={[
-        styles.board,
-        turn == "b" ? styles.tb : styles.tw,
-        inCheck ? styles.inCheck : "",
-      ].join(" ")}
-    >
-      {ranks.map((rank, i) => (
-        <div className={styles.row} key={i}>
-          {files.map((file, j) => (
-            <Cell
-              key={`${i},${j}`}
-              {...{
-                piece: pieces[i][j],
-                square: file + rank,
-                handleCellClick,
-                highlighted,
-                turn,
-                isWhite: (i + j) % 2 == 0,
+    <>
+      <div className={[styles.container, styles.chessPage].join(" ")}>
+        <div className={styles.board}>
+          <div className={styles.btnGroup}>
+            <button onClick={easy} className={easyBtn}>
+              Easy
+            </button>
+            <button onClick={medium} className={mediumBtn}>
+              Medium
+            </button>
+            <button onClick={hard} className={hardBtn}>
+              Hard
+            </button>
+          </div>
+
+          <div className={inCheckAlert}>
+            <Chessboard
+              customDropSquareStyle={{
+                boxShadow: "inset 0 0 1px 6px rgba(255, 215, 0, 0.73)",
               }}
+              customBoardStyle={{
+                borderRadius: "5px",
+                boxShadow: "0 5px 15px rgba(0, 0, 0, 0.5 ",
+              }}
+              customDarkSquareStyle={{ backgroundColor: "#961C1C" }}
+              customLightSquareStyle={{ backgroundColor: "#fff" }}
+              position={game.fen()}
+              onPieceDrop={onDrop}
+              boardWidth={isPhone ? 333 : 800}
             />
-          ))}
+          </div>
+          <div className={styles.btnGroup}>
+            <button
+              onClick={reset}
+              className={isPhone ? styles.chessBtnBottom : styles.chessBtn}
+            >
+              Reset
+            </button>
+            <button
+              onClick={undo}
+              className={isPhone ? styles.chessBtnBottom : styles.chessBtn}
+            >
+              Undo
+            </button>
+          </div>
         </div>
-      ))}
-      <Loader hidden={!isLoading} />
-    </div>
+      </div>
+    </>
   );
 };
 
